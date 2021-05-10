e120799f4377041da2045c2f14cc38803ee71e9c
maria-papachristopoulou <mar.papapchristopoulou@gmail.com>
2021-05-10 21:55:27 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.banner_slider').slick({
+	autoplay:true,
+	  autoplaySpeed:3000,
+	  dots: true,
+	  infinite: true,
+	  speed: 1000,
+	  slidesToShow: 1,
+	  adaptiveHeight: true,
+	arrows : false
+});
